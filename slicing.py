5087ee2e3920e3f019e26ebeae2a9d9d639c735f
ravi833 <107167422+ravi833@users.noreply.github.com>
2022-06-24 11:49:44 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,69 @@
+'''slicing practice on strings'''
+# a='resume'
+# print(a[:])  #resume
+# print(a[:4])  #resu
+# print(a[1:4])  # esum
+# print(a[5:4])  #blank line
+# print(a[3:2])  #blank line
+
+# a='virat kohli'
+# print(a[2:4])   #ra
+# print(a[0:8])  # virat ko
+# print(a[3:6])  #at
+# print(a[:9])    #virat koh
+# print(a[2:6])  # rat
+
+
+# a='abu dhabi'
+# print(a[0:4])  #abu
+# print(a[4:4])   #blank line
+# print(a[5:])  #habi
+# print(a[5:4])  #blank line
+
+
+# c='vikram'
+# print(c[0:5]) #vikra
+# print(c[2:5]) #kra
+# print(c[0:]) #vikram
+# print(c[:])   #vikram
+
+
+'''slicing practice on strings part 2'''
+
+a='babuanna'
+# print(a[:-1])  #babuann
+# print(a[-1:])  #a
+# print(a[-2:])  #na
+# print(a[-3:])  #nna
+# print(a[-4:])  #anna
+# print(a[-8:])   #babuanna
+# print(a[-6:6])    #buan
+# print(a[-5:4])   #u
+
+
+'''double colon'''
+
+# a='gowthamkuma'
+# print(a[::-1])  #amukmahtwog
+# print(a[::-2])  #aumhwg
+# print(a[::-3])  #akho
+# print(a[::-4])    #amw
+# print(a[::-5])    #agg
+# print(a[::-6])    #ah
+# print(a[::-12])  #a
+
+
+a='spokenenglish'
+# print(a[1::5])  #pes
+# print(a[::1])  #spokenenglish
+# print(a[::2])  #soeegin
+# print(a[::3])  #skelh
+# print(a[::4])   #segh 
+# print(a[::5])  #sni
+# print(a[::10])  #si
+# print(a[4::2])  #eegih
+# print(a[3:7:4])  #k
+# print(a[2:4:7])  #o
+# print(a[-1:5:])  #blank line
+# print(a[-8:6:2])  #n
+# print(a[5:1:-2])   #nk
